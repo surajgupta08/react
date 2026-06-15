4e892c32a12d5763caa41150a7dd1707aea53ea6
HP <surajaggarwal62068@gmail.com>
2026-06-15 11:19:43 +0530

    created a presonal render function

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Myapp() {
+  return <h3>chai aur code</h3>;
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   Children: "Click me to visit google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    visit google
+  </a>
+);
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click me to visit google",
+);
+
+createRoot(document.getElementById("root")).render(reactElement);
